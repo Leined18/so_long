9d4a862bc7cec3d22eeda972a60372821303b043
Daniel <erdanielmarciano@hotmail.com>
2024-05-07 12:58:34 +0200

    so_long .h

---
+++ Makefile
@@ -0,0 +1,6 @@
+NAME	= so_long.a
+
+CC		= cc
+CCFLAGS = -Wextra -Werror -Wall
+
+MLXFLAG = -lmlx -lXext -lX11
---
+++ inc/so_long.h
@@ -0,0 +1,9 @@
+#ifndef SO_LONG_H
+#   define SO_LONG_H
+
+#   include <unistd.h>
+#   include <fcntl.h>
+
+#   include "mlx.h"  // Incluir la biblioteca miniblx
+
+#endif /* SO_LONG_H */
